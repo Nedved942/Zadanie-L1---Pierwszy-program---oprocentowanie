56cf009c924876d3444c61a2900e26d64cc32f5f
nedved942 <pawelkoziol77@wp.pl>
2023-09-01 22:18:05 +0200

    Zaimplementowanie pierwszych zmiennych

---
+++ main.py
@@ -0,0 +1,11 @@
+print("\n*** Pierwszy program - oprocentowanie ***")
+
+# initial_credit_value = input("Podaj początkową wartość kredytu: ")
+# interest_rate = input("Podaj oprocentowanie kredytu: ")
+# loan_installment = input("Podaj kwotę raty kredytu: ")
+
+initial_credit_value = 12000
+interest_rate = 3
+loan_installment = 200
+
+print(f"Twoja pozostała kwota kredytu to {}, to {} mniej niż w poprzednim miesiącu.")
